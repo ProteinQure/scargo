b46ff2cbf10cacf0629072695bca10ea6f8a5d19
Sean Aubin <sean@proteinqure.com>
2021-02-04 15:07:02 -0500

    make example runnable as unit test

---
+++ scargo/tests/test_examples.py
@@ -0,0 +1,19 @@
+import pytest
+
+from scargo.paths import EXAMPLES_DIR, PKG_ROOT_DIR
+
+
+@pytest.mark.script_launch_mode("subprocess")
+@pytest.mark.parametrize(
+    "script_path",
+    [
+        EXAMPLES_DIR / "python_step" / "python_step.py",
+        EXAMPLES_DIR / "multi_step" / "multi_step.py",
+        EXAMPLES_DIR / "multi_step_with_condition" / "multi_step_with_cond.py",
+    ],
+)
+def test_run_examples(script_runner, script_path):
+
+    result = script_runner.run("python", script_path, env={"SCARGO_LOCAL_MOUNT": PKG_ROOT_DIR})
+    assert result.success
+    assert result.stderr == ""
