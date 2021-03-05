daea3c2260aea6f720e717cb9e35ec0ac89454f8
Sean Aubin <sean@proteinqure.com>
2021-03-05 10:27:54 -0500

    parametrize resolve_string test

--- scargo/transpile/tests/test_transformer.py
+++ scargo/transpile/tests/test_transformer.py
@@ -1,26 +1,29 @@
 import ast
 
+import pytest
+
 from scargo.transpile.transformer import SourceToArgoTransformer
 
 
-def test_resolve_string():
+@pytest.mark.parametrize("path_value, ext", [("add_alpha_", ".txt"), ("add_beta_", ".csv")])
+def test_resolve_string(path_value, ext):
     """
     Testing if a JoinedStr node representing an f-string is correctly resolved
     by the SourceToArgoTransformer.
     """
     node = ast.JoinedStr(
         values=[
-            ast.Constant(value="add_alpha_", kind=None),
+            ast.Constant(value=path_value, kind=None),
             ast.FormattedValue(
                 value=ast.Constant(value="{{inputs.parameters.init-value}}", kind=None),
                 conversion=-1,
                 format_spec=None,
             ),
-            ast.Constant(value=".txt", kind=None),
+            ast.Constant(value=ext, kind=None),
         ],
     )
 
-    assert SourceToArgoTransformer._resolve_string(node) == r"add_alpha_{{inputs.parameters.init-value}}.txt"
+    assert SourceToArgoTransformer._resolve_string(node) == "%s{{inputs.parameters.init-value}}%s" % (path_value, ext)
 
 
 def test_resolve_subscript():
