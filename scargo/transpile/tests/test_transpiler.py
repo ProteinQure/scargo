254e168202356c9b1d800303a71c890c1e87cf02
Sean Aubin <sean@proteinqure.com>
2021-03-23 13:10:58 -0400

    Add tests for small transpiler functions

--- scargo/transpile/tests/test_transpiler.py
+++ scargo/transpile/tests/test_transpiler.py
@@ -2,8 +2,12 @@
 Testing the Transpiler classes.
 """
 
+import ast
 from pathlib import Path
 
+import pytest
+
+from scargo.core import MountPoint, MountPoints, WorkflowParams
 from scargo.transpile import transpiler
 
 
@@ -27,3 +31,43 @@ def test_transpile_parameters(scargo_workflow_params_file):
     }
 
     assert dict(workflow_params) == expected_workflow_params
+
+
+@pytest.mark.parametrize(
+    "script_locals, expected",
+    [
+        (
+            {
+                "workflow_parameters": WorkflowParams(
+                    {"s3-bucket": "pq-dataxfer-tmp", "input-val": "1", "output-path": "testing/scargo-examples/output"}
+                ),
+                "mount_points": MountPoints(
+                    {"root": MountPoint(local=Path("/home/sean/git/scargo/examples/data"), remote="pq-dataxfer-tmp")}
+                ),
+            },
+            {"root": "pq-dataxfer-tmp"},
+        )
+    ],
+)
+def test_mount_points_from_locals(script_locals, expected):
+    assert transpiler.mount_points_from_locals(script_locals) == expected
+
+
+@pytest.mark.parametrize(
+    "decorators, expected",
+    [
+        (
+            [
+                ast.Call(
+                    func=ast.Name(id="scargo", ctx=ast.Load()),
+                    args=[],
+                    keywords=[ast.keyword(arg="image", value=ast.Constant(value="python:alpine"))],
+                )
+            ],
+            ["scargo"],
+        ),
+        ([ast.Name(id="entrypoint", ctx=ast.Load())], ["entrypoint"]),
+    ],
+)
+def test_get_decorator_names(decorators, expected):
+    assert transpiler.get_decorator_names(decorators) == expected
